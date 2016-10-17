9d471051a03a79a3450235c79eed32f5a46c3aa5
Ian Hayes <ian@Ians-MacBook-Pro.local>
2016-10-17 08:59:13 -0400

    testing the display of department name in profile show page

--- app/controllers/my_profile_controller.rb
+++ app/controllers/my_profile_controller.rb
@@ -2,6 +2,8 @@ class MyProfileController < ApplicationController
   before_action :authenticate_user!
   def show
     @user = current_user
+    # @department = @user.departments
+    # @department = current_user.departments
   end
 
 end
