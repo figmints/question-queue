74ecbea58001e72d839eb4ec414e12d32abfe995
Ian Hayes <ian@Ians-MacBook-Pro.local>
2016-10-19 12:30:32 -0400

    fixing relationship between user and department to display department on profile page

--- app/controllers/my_profile_controller.rb
+++ app/controllers/my_profile_controller.rb
@@ -2,8 +2,6 @@ class MyProfileController < ApplicationController
   before_action :authenticate_user!
   def show
     @user = current_user
-    # @department = @user.departments
-    # @department = current_user.departments
   end
 
 end
--- app/models/department.rb
+++ app/models/department.rb
@@ -1,5 +1,5 @@
 class Department < ActiveRecord::Base
   belongs_to :question
-  belongs_to :user
+  has_one :user
 
 end
