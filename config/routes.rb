461cbb1a5ca8a41e9ffd9381c347055a7aa78658
Ian Hayes <ian@Ians-MacBook-Pro.local>
2017-08-02 16:03:04 -0400

    adding display of questions on profile

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,16 @@ Rails.application.routes.draw do
 
   resources :search, only: [:index]
 
-  get 'my_profile', to: :show, controller: 'my_profile'
-  get 'my_profile/question', to: 'my_profile#question'
-  resources :profiles
+  get 'my_profile', action: :show, controller: 'my_profile'
+  # resources :my_profile
+  namespace :my_profile do
+    resources :profile_questions, only: [:index]
+  end
+  # resources :my_profile do
+    # resources :profile_questions, only: [:index]
+    # get 'my_profile/questions', to: :index, controller: 'profile_questions'
+    # resources :questions, controller: 'profile_questions_controller', only: [:index]
+  # end
 
   # resources :my_profile
 
